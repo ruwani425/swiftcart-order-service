06e6a6cb78b34dd0d5dacaa03a6684a99983b53c
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 22:51:59 +0530

    fix: clean OrderServiceApplication main class

--- src/main/java/com/eca/backend/orderservice/OrderServiceApplication.java
+++ src/main/java/com/eca/backend/orderservice/OrderServiceApplication.java
@@ -3,11 +3,9 @@ package com.eca.backend.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = "com.eca.backend.orderservice")
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
